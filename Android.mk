4c34cc46166964f4440ccfac20de057f53351df7
Varun Date <date.varun123@gmail.com>
2020-09-29 17:01:38 +1000

    Wallpapers: make it buildable
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- Android.mk
+++ Android.mk
@@ -21,6 +21,8 @@ LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
 LOCAL_PRODUCT_MODULE := true
 LOCAL_PRIVILEGED_MODULE := false
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
 LOCAL_PACKAGE_NAME := RRWallpapers
 
