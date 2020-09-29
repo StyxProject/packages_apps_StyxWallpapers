5af2d6f1e2e2c1f301cb895ca40edb340e5110ed
Varun Date <date.varun123@gmail.com>
2020-09-29 16:11:30 +1000

    Bringup Wallpapers for Q
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- Android.mk
+++ Android.mk
@@ -17,9 +17,10 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
-
-LOCAL_MODULE_TAGS := optional
+LOCAL_SDK_VERSION := current
+LOCAL_CERTIFICATE := platform
+LOCAL_PRODUCT_MODULE := true
+LOCAL_PRIVILEGED_MODULE := false
 
 LOCAL_PACKAGE_NAME := RRWallpapers
 
